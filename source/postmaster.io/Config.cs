4aa04a9b341cc744b02f6c54de4ac8627c6e10c0
Zach Harbort <zach.harbort@gmail.com>
2013-08-18 16:33:36 -0500

    Add Config.cs. Windows store doesn't support System.Configuration

---
+++ source/postmaster.io/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace Postmaster.io
+{
+    /// <summary>
+    /// Config.
+    /// </summary>
+    public class Config
+    {
+        public const string BaseUri = "https://api.postmaster.io";
+        public const string Version = "V1";
+        public const string ApiKey = "";
+        public const string Password = "";
+    }
+}
