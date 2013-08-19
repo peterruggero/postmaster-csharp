8a52c0d0c3fc5dac27958616cd7be0f726db3ac3
Zach Harbort <zach.harbort@gmail.com>
2013-08-19 00:08:25 -0500

    Add test project for unit testing

---
+++ source/Postmaster.io.test/UnitTestCreateShipment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Postmaster.io.Api.V1.Entities.Shipment;
+
+namespace Postmaster.io.test
+{
+    [TestClass]
+    public class UnitTestCreateShipment
+    {
+        [TestMethod]
+        public void CreateShipment()
+        {
+            // build object
+            Shipment shipment = new Shipment
+            {
+
+            };
+
+            // post object
+
+            // read response
+        }
+    }
+}
